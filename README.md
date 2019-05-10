74c7a8ded04b9f056ae41fcdb2f551ad50ff2135
Moxious <moxious@oldhat.org>
2019-05-10 16:33:25 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Plumbline
+
+TBD
---
+++ schema.js
@@ -0,0 +1,18 @@
+import { neo4jgraphql } from 'neo4j-graphql-js';
+
+export const typeDefs = `
+type User {
+  id: ID!
+  name: String
+  posts: [Post] @relation(name: "POSTED", direction: "OUT")
+}
+
+type Post {
+  id: ID!
+  name: String
+  content: String
+  posted_by: User @relation(name: "POSTED", direction: "IN")
+}
+`;
+
+export const resolvers = { };
